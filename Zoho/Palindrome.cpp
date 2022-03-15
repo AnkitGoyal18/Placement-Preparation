40e9dc34202870751329b189fe37b17877d74601
Ankit Goyal <ankitgoyal21302@gmail.com>
2022-03-15 18:25:07 +0530

    #Day1 Zoho

---
+++ Zoho/Palindrome.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string is_palindrome(int n)
+{
+    int rev = 0, temp = n;
+    while (temp != 0)
+    {
+        rev = rev * 10 + temp % 10;
+        temp /= 10;
+    }
+
+    return rev == n ? "Yes" : "No";
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << is_palindrome(n);
+
+    return 0;
+}
